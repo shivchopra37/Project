a02a5831978474129ddc0867966968e4002b32a8
Sushant-4444 <arorasushant4444@gmail.com>
2024-02-09 01:40:59 +0530

    footer'

--- PROJECT/bookverse/src/App.js
+++ PROJECT/bookverse/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/header';
+import Footer from './Components/Footer/footer';
 
 function App() {
   return (
     <>
     <Header></Header>
+    <Footer></Footer>
     </>
   );
 }
--- PROJECT/bookverse/src/Components/Header/header.js
+++ PROJECT/bookverse/src/Components/Header/header.js
@@ -16,7 +16,7 @@ const Header=()=>{
     </div>
   </nav>
   </div>
-    </>);
+    </>)
 
     
 }
