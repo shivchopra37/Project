2bcd8d0c54b6cabeb95b026b3bd8de465f95cf68
Sushant-4444 <arorasushant4444@gmail.com>
2024-02-09 04:43:46 +0530

    final

--- PROJECT/bookverse/src/Components/Header/header.js
+++ PROJECT/bookverse/src/Components/Header/header.js
@@ -8,11 +8,11 @@ const Header=()=>{
     <div className="logo">BookVerse</div>
     <div className="other_than_logo">
     <ul className="nav-links">
-      <li><Link to="#">Home</Link></li>
-      <li><Link to="#">Sell/Donate</Link></li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/form">Sell/Donate</Link></li>
       <li><Link to="/buy">Buy/Exchange</Link></li>
-      <li><Link to="#">E-Books</Link></li>
-      <li><Link to="#">Audio-Books</Link></li>
+      <li><Link to="/buy">E-Books</Link></li>
+      <li><Link to="/buy">Audio-Books</Link></li>
     </ul>
     </div>
   </nav>
