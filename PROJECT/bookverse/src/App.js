d37c71170475f31166dcae35171ae93141464a9c
arpan21020 <arpan21020@iiitd.ac.in>
2024-02-09 04:22:39 +0530

    api added

--- PROJECT/bookverse/src/App.js
+++ PROJECT/bookverse/src/App.js
@@ -23,12 +23,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/buy" element={<BuyExchange />} />
-<<<<<<< HEAD
+        <Route path="/books/sample" element={<BookPage image="/Images/111.jpg" price="Rs 200" name="The Great Gatsby" author="F. Scott Fitzgerald" description="Set in the summer of 1922, The Great Gatsby by F. Scott Fitzgerald revolves around the enigmatic Jay Gatsby and his infatuation with the alluring Daisy Buchanan. The story unfolds in the fictional towns of West Egg and East Egg on Long Island, depicting the opulent yet morally bankrupt lives of the wealthy elite during the Jazz Age. Gatsby's lavish parties serve as a facade to his deeply rooted desire to reclaim his lost love, Daisy, who is married to the affluent but morally corrupt Tom Buchanan. Through vivid imagery and intricate character dynamics, Fitzgerald explores themes of love, wealth, and the pursuit of the American Dream. The tragic narrative culminates in a poignant examination of the consequences of obsession and the fleeting nature of happiness." genre="Fiction, Classic"/>} />
         <Route path="/review" element={<ReviewsSection/>}/>
-=======
-        <Route path="/sample" element={<BookPage />} />
-
->>>>>>> f832f5bd49d03e1a4276e40efcca3b09d73b2c88
       </Routes>
       <Footer />
     </Router>
--- PROJECT/bookverse/src/Components/BookPage/bookpage.js
+++ PROJECT/bookverse/src/Components/BookPage/bookpage.js
@@ -1,9 +1,66 @@
-const BookPage=()=>{
+import './bookpage.css'
+import React, { useState, useEffect } from 'react';
+const BookPage=(props)=>{
+    const [inputValue, setInputValue] = useState('');
+    const [array, setArray] = useState(["Nice Book"]);
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+    const handleAddToArray = () => {
+
+        fetch('https://community-purgomalum.p.rapidapi.com/json', {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-host': 'community-purgomalum.p.rapidapi.com',
+            'x-rapidapi-key': '91ed35f487mshb3b16b3df9c6255p1a9a88jsn5bae293bbf7b', // replace your RapidAPI key
+        },
+    })
+    .then(response => response.json())
+    .then(data => setArray(prevArray => [...prevArray,JSON.stringify(data) ]) // assuming data is the value you want to add to the array
+    )
+    .catch(err => console.error(err));
+    };
     return(
         <>
         <div className="desc-container">
-            
+        <div className="book-image">
+                    <img
+                        src={props.image}
+                        alt=""
+                        useMap="#image_map"
+                        style={{
+                            width: '400px',
+                            height: '500px',
+                            backgroundSize: 'cover',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundPosition: 'center',
+                            // height:"30px",
+                        }}
+                    />
+                </div>
+                <div className="book-details">
+                    <h1>{props.name}</h1>
+                    <p>Author : {props.author}</p>
+                    <p className='description'>Description : {props.description}</p>
+                    <p>Price : {props.price}</p>
+                    {/* <p>Condition</p> */}
+                </div>
         </div>
+        <div className='reviews'>
+            <input type="text" value={inputValue} onChange={handleChange} />
+            <button onClick={handleAddToArray}>Submit</button>
+            <p>The current input value is: {inputValue}</p>
+        </div>
+        <div className="reviews2">
+            {array.map((item, index) => (
+                <div key={index}>
+                    Review  #{index + 1}: {item}
+                    </div>
+            ))}
+        </div>
+        
+
         </>
 
     )
