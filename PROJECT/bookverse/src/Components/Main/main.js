145a861a0188ceeeb373f1a6522fe6243f4c0a3b
arpan21020 <arpan21020@iiitd.ac.in>
2024-02-09 01:14:32 +0530

    Main

---
+++ PROJECT/bookverse/src/Components/Main/main.js
@@ -0,0 +1,29 @@
+
+const Main=()=>{
+    return(
+        <div className="container">
+            <div className="left">
+                <div className="c1">
+                    <p>There is only thing that could replace a Book. The Next Book.</p>
+                </div>
+                <div className="c2">
+                    <h1>
+                        Discover, Find, Share Your Books
+                    </h1>
+                </div>
+                <div className="c3">
+                    <ul>
+                        <li>From Fictional to Education we got you covered</li>
+                        <li>All Genres availabel</li>
+                    </ul>
+                </div>
+                <div className="c4">
+                    <Link></Link>
+                </div>
+            </div>
+            <div className="right"></div>
+            
+        </div>
+    )
+}
+export default Main;
