fd93e1cd1ee141a0e05685e45e13f5eda3d81d10
Sushant-4444 <arorasushant4444@gmail.com>
2024-02-09 00:15:29 +0530

    using react

--- PROJECT/Script.js
+++ PROJECT/Script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+async function api1(){
+    const response=await fetch("https://cat-fact.herokuapp.com/facts");
+    console.log(response);
+    const data=await response.json();
+    console.log(data);
+    return data[4]["text"];
+}
+const text=api1();
+let element=document.querySelector("p");
+async function display(){
+ element.innerText=await api1();
+}
+display();
