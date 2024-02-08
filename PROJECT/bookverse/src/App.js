ba1ec5ce754e3b0d93805535b71d5c1175669827
arpan21020 <arpan21020@iiitd.ac.in>
2024-02-09 01:38:32 +0530

    design

--- PROJECT/bookverse/src/App.js
+++ PROJECT/bookverse/src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/header';
-
+import Main from './Components/Main/main';
+import {
+  HashRouter as Router,
+  RouterProvider,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
 function App() {
   return (
     <>
-    <Header></Header>
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
     </>
   );
 }
